f5612c653812cd0613592e8769be1b15443c4b92
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2021-02-03 14:05:42 -0300

    Função que verifica o empate/ fim de jogo foi adicionada

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -3,6 +3,9 @@ _SUCESSO = 0
 _VAZIO = ' '
 _TABULEIRO_LINHA = 3
 _TABULEIRO_COLUNA = 3
+_PLAYER_1_GANHOU = 1
+_PLAYER_2_GANHOU = 2
+_EMPATE = 3
 
 class Tabuleiro:
     def __init__(self):
@@ -32,28 +35,33 @@ class Tabuleiro:
         for linha in range(_TABULEIRO_LINHA):
             for coluna in range(_TABULEIRO_COLUNA):
                 if(self.__tabuleiro[linha][coluna] == _VAZIO):
-                    return 1
-        return 0
+                    return True
+        return False
 
     def verificarGanhador(self, p1, p2):
         #   Jogador 1
         #   Linha
         for i in range(_TABULEIRO_LINHA):
-            if(self.__tabuleiro[i][0] == p1 and self.__tabuleiro[i][1] == p1 and self.__tabuleiro[i][2] == p1): return 1
+            if(self.__tabuleiro[i][0] == p1 and self.__tabuleiro[i][1] == p1 and self.__tabuleiro[i][2] == p1): return _PLAYER_1_GANHOU
         #   Coluna
         for i in range(_TABULEIRO_COLUNA):
-            if(self.__tabuleiro[0][i] == p1 and self.__tabuleiro[1][i] == p1 and self.__tabuleiro[2][i] == p1): return 1
+            if(self.__tabuleiro[0][i] == p1 and self.__tabuleiro[1][i] == p1 and self.__tabuleiro[2][i] == p1): return _PLAYER_1_GANHOU
         #   Diagonal
-        if(self.__tabuleiro[0][0] == p1 and self.__tabuleiro[1][1] == p1 and self.__tabuleiro[2][2] == p1): return 1
-        if(self.__tabuleiro[2][0] == p1 and self.__tabuleiro[1][1] == p1 and self.__tabuleiro[0][2] == p1): return 1
+        if(self.__tabuleiro[0][0] == p1 and self.__tabuleiro[1][1] == p1 and self.__tabuleiro[2][2] == p1): return _PLAYER_1_GANHOU
+        if(self.__tabuleiro[2][0] == p1 and self.__tabuleiro[1][1] == p1 and self.__tabuleiro[0][2] == p1): return _PLAYER_1_GANHOU
 
         #   Jogador 2
         #   Linha
         for i in range(_TABULEIRO_LINHA):
-            if(self.__tabuleiro[i][0] == p2 and self.__tabuleiro[i][1] == p2 and self.__tabuleiro[i][2] == p2): return 2
+            if(self.__tabuleiro[i][0] == p2 and self.__tabuleiro[i][1] == p2 and self.__tabuleiro[i][2] == p2): return _PLAYER_2_GANHOU
         #   Coluna
         for i in range(_TABULEIRO_COLUNA):
-            if(self.__tabuleiro[0][i] == p2 and self.__tabuleiro[1][i] == p2 and self.__tabuleiro[2][i] == p2): return 2
+            if(self.__tabuleiro[0][i] == p2 and self.__tabuleiro[1][i] == p2 and self.__tabuleiro[2][i] == p2): return _PLAYER_2_GANHOU
         #   Diagonal
-        if(self.__tabuleiro[0][0] == p2 and self.__tabuleiro[1][1] == p2 and self.__tabuleiro[2][2] == p2): return 2
-        if(self.__tabuleiro[2][0] == p2 and self.__tabuleiro[1][1] == p2 and self.__tabuleiro[0][2] == p2): return 2
+        if(self.__tabuleiro[0][0] == p2 and self.__tabuleiro[1][1] == p2 and self.__tabuleiro[2][2] == p2): return _PLAYER_2_GANHOU
+        if(self.__tabuleiro[2][0] == p2 and self.__tabuleiro[1][1] == p2 and self.__tabuleiro[0][2] == p2): return _PLAYER_2_GANHOU
+
+        if(self.__verificarPosicaoVazia()):
+            return False
+        
+        return _EMPATE
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from Tabuleiro import Tabuleiro
+from Tabuleiro import Tabuleiro, _PLAYER_1_GANHOU, _PLAYER_2_GANHOU, _EMPATE
 from Player import Player
 
 tabuleiro = Tabuleiro()
 j1 = Player('Fulano', 'O')
 j2 = Player('Ciclano', 'X')
 vez = 1
-while(vez < 10):
+vitoria = 0
+while(True):
     tabuleiro.printarTabuleiro()
     if(vez % 2 != 0):
         print('Jogador 1')
         j1.fazerJogada()
-        if(tabuleiro.fazerJogada(j1.linha, j1.coluna, j1.simbolo)):
+        if(not tabuleiro.fazerJogada(j1.linha, j1.coluna, j1.simbolo)):
             vez += 1
     else:
         print('Jogador 2')
         j2.fazerJogada()
-        if(tabuleiro.fazerJogada(j2.linha, j2.coluna, j2.simbolo)):
+        if(not tabuleiro.fazerJogada(j2.linha, j2.coluna, j2.simbolo)):
             vez += 1
+    vitoria = tabuleiro.verificarGanhador(j1.simbolo, j2.simbolo)
+    if(vitoria):
+        if(vitoria == _PLAYER_1_GANHOU):
+            print('Jogador 1 ganhou!')
+            break
+        elif(vitoria == _PLAYER_2_GANHOU):
+            print('jogador 2 ganhou!')
+            break
+        else:
+            print('Empate!')
+            break
     print('---------------------------')
     print('---------------------------')
     print('---------------------------')
