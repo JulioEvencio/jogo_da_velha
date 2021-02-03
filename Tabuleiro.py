d6adfa0971f4a9e030915392bdb79196c62f5c4a
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2021-02-03 13:51:13 -0300

    Função que verifica uma posição vazia no tabuleiro foi adicionada

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,6 +1,8 @@
 _ERRO = -1
 _SUCESSO = 0
 _VAZIO = ' '
+_TABULEIRO_LINHA = 3
+_TABULEIRO_COLUNA = 3
 
 class Tabuleiro:
     def __init__(self):
@@ -26,13 +28,20 @@ class Tabuleiro:
         self.__tabuleiro[linha][coluna] = simbolo
         return _SUCESSO
 
+    def __verificarPosicaoVazia(self):
+        for linha in range(_TABULEIRO_LINHA):
+            for coluna in range(_TABULEIRO_COLUNA):
+                if(self.__tabuleiro[linha][coluna] == _VAZIO):
+                    return 1
+        return 0
+
     def verificarGanhador(self, p1, p2):
         #   Jogador 1
         #   Linha
-        for i in range(3):
+        for i in range(_TABULEIRO_LINHA):
             if(self.__tabuleiro[i][0] == p1 and self.__tabuleiro[i][1] == p1 and self.__tabuleiro[i][2] == p1): return 1
         #   Coluna
-        for i in range(3):
+        for i in range(_TABULEIRO_COLUNA):
             if(self.__tabuleiro[0][i] == p1 and self.__tabuleiro[1][i] == p1 and self.__tabuleiro[2][i] == p1): return 1
         #   Diagonal
         if(self.__tabuleiro[0][0] == p1 and self.__tabuleiro[1][1] == p1 and self.__tabuleiro[2][2] == p1): return 1
@@ -40,10 +49,10 @@ class Tabuleiro:
 
         #   Jogador 2
         #   Linha
-        for i in range(3):
+        for i in range(_TABULEIRO_LINHA):
             if(self.__tabuleiro[i][0] == p2 and self.__tabuleiro[i][1] == p2 and self.__tabuleiro[i][2] == p2): return 2
         #   Coluna
-        for i in range(3):
+        for i in range(_TABULEIRO_COLUNA):
             if(self.__tabuleiro[0][i] == p2 and self.__tabuleiro[1][i] == p2 and self.__tabuleiro[2][i] == p2): return 2
         #   Diagonal
         if(self.__tabuleiro[0][0] == p2 and self.__tabuleiro[1][1] == p2 and self.__tabuleiro[2][2] == p2): return 2
