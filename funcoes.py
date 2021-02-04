77948e754fdd07dc51125585d151b0387418486b
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2021-02-04 14:27:20 -0300

    Função resposável pelo jogo em si foi adicionada

---
+++ funcoes.py
@@ -0,0 +1,45 @@
+from Tabuleiro import Tabuleiro
+
+
+import os
+from Tabuleiro import _PLAYER_1_GANHOU, _PLAYER_2_GANHOU
+
+def jogar(tabuleiro, j1, j2):
+    vez = 1
+    vitoria = 0
+    while(True):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        tabuleiro.printarTabuleiro()
+        if(vez % 2 != 0):
+            print('Jogador 1')
+            j1.fazerJogada()
+            if(tabuleiro.fazerJogada(j1.linha, j1.coluna, j1.simbolo)):
+                print('Posição inválida!')
+                print('---------------------------')
+                input('Pressione enter para continuar...')
+            else:
+                vez += 1
+        else:
+            print('Jogador 2')
+            j2.fazerJogada()
+            if(tabuleiro.fazerJogada(j2.linha, j2.coluna, j2.simbolo)):
+                print('Posição inválida!')
+                print('---------------------------')
+                input('Pressione enter para continuar...')
+            else:
+                vez += 1
+        vitoria = tabuleiro.verificarGanhador(j1.simbolo, j2.simbolo)
+        if(vitoria):
+            if(vitoria == _PLAYER_1_GANHOU):
+                os.system('cls' if os.name == 'nt' else 'clear')
+                tabuleiro.printarTabuleiro()
+                print('Jogador 1 ganhou!')
+                break
+            elif(vitoria == _PLAYER_2_GANHOU):
+                os.system('cls' if os.name == 'nt' else 'clear')
+                tabuleiro.printarTabuleiro()
+                print('jogador 2 ganhou!')
+                break
+            else:
+                print('Empate!')
+                break
